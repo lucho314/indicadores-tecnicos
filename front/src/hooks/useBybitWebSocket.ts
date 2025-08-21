186a1bf9d1f8388a19de5890b63963109c07f9a1
Luciano Zapata <lzapata@ater.gob.ar>
2025-08-21 00:41:23 -0300

    Refactor: Actualizar la URL del WebSocket para usar la dirección IP en lugar de localhost

--- front/src/hooks/useBybitWebSocket.ts
+++ front/src/hooks/useBybitWebSocket.ts
@@ -1,7 +1,7 @@
 "use client"
 
 import { useState, useEffect, useRef, useCallback } from "react"
-const API_BASE_URL = import.meta.env.VITE_API_URL  || "http://localhost:8000"
+const API_BASE_URL = import.meta.env.VITE_API_URL  || "134.65.253.0:8000"
 export interface WebSocketPositionData {
   symbol: string
   timestamp: string
